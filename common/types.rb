afad62a78d02d839d50b3fd3e5c753629ef4fe80
Adrien Toubiana <adrientoub@hotmail.com>
2020-04-16 01:36:09 +0200

    Instagram: Parse messages into common format

---
+++ common/types.rb
@@ -0,0 +1,75 @@
+require 'json'
+require 'date'
+
+CURRENT_VERSION = '0.0.3'
+
+class Index
+  attr_accessor :version, :conversations
+
+  def self.from_json(json)
+    index = new
+    index.version = json['version']
+    index.conversations = json['conversations'].map do |conversation|
+      ConversationIndex.from_json(conversation)
+    end
+    index
+  end
+end
+
+class ConversationIndex
+  attr_accessor :conversation_name, :path, :message_count
+
+  def self.from_json(json)
+    conversation = new
+    conversation.conversation_name = json['conversation_name']
+    conversation.path = json['path']
+    conversation.message_count = json['message_count']
+    conversation
+  end
+
+  def load_conversation
+    content = File.read(self.path)
+    json = JSON.parse(content)
+    ConversationFile.from_json(json)
+  end
+end
+
+class ConversationFile
+  attr_accessor :conversation_name, :participants, :messages
+
+  def self.from_json(json)
+    conversation = new
+    conversation.conversation_name = json['conversation_name']
+    conversation.participants = json['participants']
+    conversation.messages = json['messages'].map do |message|
+      Message.from_json(message)
+    end
+    conversation
+  end
+end
+
+class Message
+  attr_accessor :sender, :content, :date, :reactions
+
+  def self.from_json(json)
+    message = new
+    message.sender = json['sender']
+    message.content = json['content']
+    message.date = DateTime.parse(json['date'])
+    message.reactions = json['reactions']&.map do |reaction|
+      Reaction.from_json(reaction)
+    end
+    message
+  end
+end
+
+class Reaction
+  attr_accessor :reaction, :sender
+
+  def self.from_json(json)
+    reaction = new
+    reaction.reaction = json['reaction']
+    reaction.sender = json['sender']
+    reaction
+  end
+end
---
+++ instagram/parser.rb
@@ -0,0 +1,61 @@
+#! /usr/bin/env ruby
+
+require 'json'
+require 'date'
+require 'fileutils'
+require_relative '../common/types'
+
+filename = ARGV[0] || 'messages.json'
+
+INDEX_PATH = 'index.json'
+
+json = JSON.parse(File.read(filename))
+puts "Parsed json #{filename}"
+
+index = {
+  version: CURRENT_VERSION,
+  conversations: []
+}
+
+FileUtils.mkdir_p('conversations')
+
+json.each do |conversation|
+  conversation_name = conversation['participants'].sort.join('-')
+  path = "conversations/#{conversation_name}.json"
+
+  conv_raw = {
+    conversation_name: conversation_name,
+    participants: conversation['participants'],
+    messages: conversation['conversation'].map do |message|
+      {
+        sender: message['sender'],
+        date: message['created_at'],
+        content: message['text'],
+        reactions: message['likes']&.map do |like|
+          {
+            sender: like['username'],
+            reaction: 'like'
+          }
+        end
+      }
+    end
+  }
+  File.open(path, 'w') do |file|
+    file.puts JSON.dump(conv_raw)
+  end
+
+  index_conv = {
+    conversation_name: conversation_name,
+    path: path,
+    message_count: conv_raw[:messages].count
+  }
+  index[:conversations] << index_conv
+end
+index[:conversations].sort_by! do |conversation|
+  conversation[:message_count]
+end.reverse!
+
+File.open(INDEX_PATH, 'w') do |file|
+  file.puts JSON.dump(index)
+end
+puts "Saved index in #{INDEX_PATH}"
