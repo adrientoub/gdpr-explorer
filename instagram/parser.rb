a434a520161aeefe89f9beafe08c6ee862ef7b90
Adrien Toubiana <adrientoub@hotmail.com>
2020-04-16 02:29:20 +0200

    Instagram: add launcher for message parsing

--- instagram/parser.rb
+++ instagram/parser.rb
@@ -5,55 +5,64 @@ require 'date'
 require 'fileutils'
 require_relative '../common/types'
 
-filename = ARGV[0] || 'messages.json'
+class InstagramParser
+  # Messages path is the path to the messages.json file
+  # Output path is the directory where you want to save your index.json file
+  def self.parse(messages_path, output_directory)
+    index_path = File.join(output_directory, INDEX_PATH)
 
-json = JSON.parse(File.read(filename))
-puts "Parsed json #{filename}"
+    # Create output directory
+    FileUtils.mkdir_p(File.join(output_directory, 'conversations'))
 
-index = {
-  version: CURRENT_VERSION,
-  conversations: []
-}
+    json = JSON.parse(File.read(messages_path))
+    puts "Parsed json #{messages_path}"
 
-FileUtils.mkdir_p('conversations')
+    conversations_index = []
+    index = {
+      version: CURRENT_VERSION,
+      conversations: conversations_index
+    }
 
-json.each do |conversation|
-  conversation_name = conversation['participants'].sort.join('-')
-  path = "conversations/#{conversation_name}.json"
+    json.each do |conversation|
+      conversation_name = conversation['participants'].sort.join('-')
+      output_conversation_path = "conversations/#{conversation_name}.json"
 
-  conv_raw = {
-    conversation_name: conversation_name,
-    participants: conversation['participants'],
-    messages: conversation['conversation'].map do |message|
-      {
-        sender: message['sender'],
-        date: message['created_at'],
-        content: message['text'],
-        reactions: message['likes']&.map do |like|
+      conv_raw = {
+        conversation_name: conversation_name,
+        participants: conversation['participants'],
+        messages: conversation['conversation'].map do |message|
           {
-            sender: like['username'],
-            reaction: 'like'
+            sender: message['sender'],
+            date: message['created_at'],
+            content: message['text'],
+            reactions: message['likes']&.map do |like|
+              {
+                sender: like['username'],
+                reaction: 'like'
+              }
+            end
           }
         end
       }
+      File.open(File.join(output_directory, output_conversation_path), 'w') do |file|
+        file.puts JSON.dump(conv_raw)
+      end
+
+      index_conv = {
+        conversation_name: conversation_name,
+        path: output_conversation_path,
+        message_count: conv_raw[:messages].count
+      }
+      conversations_index << index_conv
     end
-  }
-  File.open(path, 'w') do |file|
-    file.puts JSON.dump(conv_raw)
+    conversations_index.sort_by! do |conversation|
+      conversation[:message_count]
+    end.reverse!
+
+    File.open(index_path, 'w') do |file|
+      file.puts JSON.dump(index)
+    end
+    puts "Saved index in #{index_path}"
+    index
   end
-
-  index_conv = {
-    conversation_name: conversation_name,
-    path: path,
-    message_count: conv_raw[:messages].count
-  }
-  index[:conversations] << index_conv
 end
-index[:conversations].sort_by! do |conversation|
-  conversation[:message_count]
-end.reverse!
-
-File.open(INDEX_PATH, 'w') do |file|
-  file.puts JSON.dump(index)
-end
-puts "Saved index in #{INDEX_PATH}"
--- launch_instagram_messages.rb
+++ launch_instagram_messages.rb
@@ -1,11 +1,11 @@
 #! /usr/bin/env ruby
 
 require_relative './common/types'
-require_relative './facebook/parser'
+require_relative './instagram/parser'
 
 def print_help
-  puts "Usage: #{__FILE__ } [path_to_inbox] [output_directory]"
-  puts "  'path_to_inbox' is the path to the Facebook Archive folder named 'messages/inbox'"
+  puts "Usage: #{__FILE__ } [path_to_messages] [output_directory]"
+  puts "  'path_to_messages' is the path to the Instagram file named 'messages.json'"
   puts "  'output_directory' is the directory where you want the script to output its work"
 end
 
@@ -13,7 +13,7 @@ if ARGV.length < 2
   print_help
 end
 
-path_to_inbox, output_directory = ARGV
+path_to_messages, output_directory = ARGV
 
 index_path = File.join(output_directory, INDEX_PATH)
 if File.exists?(index_path)
@@ -25,4 +25,4 @@ if File.exists?(index_path)
     puts "Found a viable index, reusing it."
   end
 end
-index ||= FacebookParser.parse(path_to_inbox, output_directory)
+index ||= InstagramParser.parse(path_to_messages, output_directory)
